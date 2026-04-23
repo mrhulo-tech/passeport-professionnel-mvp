d149e9cf72db1e7e5ff780f341bfaaf85d877f74
mrhulo-tech <info@hulotech.com>
2026-04-23 11:42:37 -0400

    Clone frontend base from passeport-cognitif-mvp

--- frontend/next-env.d.ts
+++ frontend/next-env.d.ts
@@ -1,1 +1,6 @@
-placeholder
+
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+// NOTE: This file should not be edited directly.
+// See https://nextjs.org/docs/basic-features/typescript for more information.
